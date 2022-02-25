4750abf9b08423be03ed0a8178a43e975291eab6
Edson Maciel <90927380+Edsonjr4@users.noreply.github.com>
2022-02-25 15:29:31 -0300

    Add files via upload

---
+++ teste 20.py
@@ -0,0 +1,10 @@
+n = 1
+soma = 0
+
+while n <= 10:
+
+    x = int(input(f'{n} número: '))
+    soma = soma + x
+    n = n + 1
+
+print(f'Soma: {soma}')
---
+++ teste 22.py
@@ -0,0 +1,8 @@
+1 print (range(10)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+2 print (range(5, 10)) # [5, 6, 7, 8, 9]
+3 print (range(10, 0)) # [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+4 i=int(input("Digite um Número"))
+5 if i in range(0,10):
+6   print("Está Contido")
+7 else:
+8   print("Não está Contido")
